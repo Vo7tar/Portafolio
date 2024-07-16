4246b5852f50fb1f25d2d19ac4c9a31ac407990c
Vo7tar <129466375+Vo7tar@users.noreply.github.com>
2024-07-15 23:24:16 -0500

    Primeros Pasos

---
+++ citas.js
@@ -0,0 +1,140 @@
+/* arrays citas Aletorias */
+
+let citas = [
+  {
+    'autor': 'Albert Einstein',
+    'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+  },
+  {
+    'autor': 'Anónimo',
+    'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+  },
+  {
+    'autor': 'Alan Kay',
+    'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+  },
+  {
+    'autor': 'Amelia Earhart',
+    'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+  },
+  {
+    'autor': 'Aristoteles',
+    'texto': 'La calidad no es un acto, es un hábito.'
+  },
+  {
+    'autor': 'Benjamin Franklin',
+    'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+  },
+  {
+    'autor': 'Charles R. Swindoll',
+    'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+  },
+  {
+    'autor': 'Jane Goodall',
+    'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+  },
+  {
+    'autor': 'John Muir',
+    'texto': 'El poder de la imaginación nos hace infinitos.'
+  },
+  {
+    'autor': 'Mark Twain',
+    'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+  },
+  {
+    'autor': 'Rounders',
+    'texto': 'Mi vida es el riesgo, lo demás es solo espera.'
+  },
+  {
+    'autor': 'Rounders',
+    'texto': 'No puedes perder lo que no has puesto sobre la mesa.'
+  },
+  {
+    'autor': 'Anonimo',
+    'texto': 'si sabes lo que vales, ve y consigue lo que mereces.'
+  },
+  {
+    'autor': 'Henry Ford',
+    'texto': 'El fracaso es una gran oportunidad para empezar otra vez con mas inteligencia.'
+  },
+  {
+    'autor': 'Anonimo',
+    'texto': '¡Cuando sientas que ese ya no es tu sitio, vuela!',
+  },
+  {
+    'autor': 'Anonymous',
+    'texto': 'You willl be the frist millionaire in your family.'
+  },
+  {
+    'autor': 'El curioso caso de Benjamin Button',
+    'texto': 'Todas las oportunidades marcan el transcurso de nuestra vida, incluso las que dejamos ir.'
+  },
+  {
+    'autor': 'Virginia Woolf',
+    'texto': 'Madurar es perder algunas ilusiones para empezar a tener otras.',
+  },
+  {
+    'autor': 'Vincent van Gogh',
+    'texto': 'Intento cada vez mas ser yo mismo, me importa relativamente poco si la gente aprueba o desaprueba.'
+  },
+  {
+    'autor': 'Jairo lambis',
+    'texto': '¡Sacúdete las dudas y dale para adelante!'
+  },
+  {
+    'autor': 'J.R.R. Tolkien',
+    'texto': 'El trabajo que nunca se empieza es el que tarda más en finalizarse.'
+  },
+  {
+    'autor': 'Anonimo',
+    'texto': 'Inténtalo una vez más.'
+  },
+  {
+    'autor': 'jeff Bezos',
+    'texto': 'Sabía que si fallaba no me arrepentiría, pero sí que me lamentaría por no intentarlo.'
+  },
+  {
+    'autor': 'Albert Camus',
+    'texto': 'La necesidad de tener razón es el signo de una mente vulgar.'
+  },
+  {
+    'autor': 'Ovidio',
+    'texto': 'Apresúrate; no te fíes de las horas venideras. El que hoy no esta dispuesto, menos lo estará mañana.'
+  },
+  {
+    'autor': 'Charles Dickens',
+    'texto': 'Cada fracaso le enseña al hombre algo que necesitaba aprender.'
+  },
+  {
+    'autor': 'Anonymous',
+    'texto': 'Risk is always better than regret.'
+  },
+  {
+    'autor': 'Sigmund Freud',
+    'texto': 'La madurez es la capacidad de posponer la gratificación.'
+  },
+  {
+    'autor': 'Anonymous',
+    'texto': 'Bad chapters can still create great stories.'
+  },
+  {
+    'autor': 'Demócrito',
+    'texto': 'El hombre fuerte es el que supera no solo a sus enemigos sino también a sus placeres.'
+  },
+  {
+    'autor': 'Rainer Maria rilke',
+    'texto': 'Deja que todo ter pase. Lo bello y lo terrible, solo sigue adelante. Ningún sentimiento es definitivo.'
+  },
+  {
+    'autor': 'Oscar Isaac',
+    'texto': 'Cuando tienes miedo a saltar es exactamente cuando saltas, sino te quedas en el mismo sitio toda tu vida.'
+  },
+  {
+    'autor': 'Janis Joplin',
+    'texto': 'Si te preocupas por tu mañana puedes destruir tu ahora.'
+  },
+  {
+    'autor': 'Jim Rohn',
+    'texto': 'Si te preocupas por tu mañana puedes destruir tu ahora.'
+  },
+];
